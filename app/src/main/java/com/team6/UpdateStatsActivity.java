8ab111619400eb0f2fa06c90b82e145beadc1cac
johnleofraxidio <johnleofraxidio@gmail.com>
2021-03-20 02:07:24 +1100

    added UpdateStatsActivity

--- app/src/main/java/com/team6/UpdateStatsActivity.java
+++ app/src/main/java/com/team6/UpdateStatsActivity.java
@@ -2,16 +2,100 @@ package com.team6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class UpdateStatsActivity extends AppCompatActivity {
+    @SuppressLint("StaticFieldLeak")
+    private static SeekBar seek_bar;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView text_view5;
+    @SuppressLint("StaticFieldLeak")
+    private static SeekBar seek_bar2;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView text_view7;
+    @SuppressLint("StaticFieldLeak")
+    private static SeekBar seek_bar3;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView text_view8;
+    @SuppressLint("StaticFieldLeak")
+    private static SeekBar seek_bar4;
+    @SuppressLint("StaticFieldLeak")
+    private static TextView text_view9;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_stats);
+        seek_bar = findViewById(R.id.seekBar);
+        text_view5 = findViewById(R.id.textView5);
+        seek_bar2 = findViewById(R.id.seekBar2);
+        text_view7 = findViewById(R.id.textView7);
+        seek_bar3 = findViewById(R.id.seekBar3);
+        text_view8 = findViewById(R.id.textView8);
+        seek_bar4 = findViewById(R.id.seekBar4);
+        text_view9 = findViewById(R.id.textView9);
+        seekBar(seek_bar,text_view5);
+        seekBar(seek_bar2,text_view7);
+        seekBar(seek_bar3,text_view8);
+        seekBar(seek_bar4,text_view9);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void seekBar(SeekBar seekbar, TextView text_view) {
+        if (text_view.equals(text_view5)) {
+            text_view.setText(seekbar.getProgress() + " oz");
+        } else if (text_view.equals(text_view7)) {
+            text_view.setText(seekbar.getProgress() + " hours");
+        } else if (text_view.equals(text_view8)) {
+            text_view.setText(seekbar.getProgress() + " hours");
+        } else if (text_view.equals(text_view9)) {
+            text_view.setText(Integer.toString(seekbar.getProgress()));
+        }
+
+        seekbar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progress_value;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progress_value = progress;
+                        if (text_view.equals(text_view5)) {
+                            text_view.setText((progress) + " oz");
+                        } else if (text_view.equals(text_view7)) {
+                            text_view.setText((progress) + " hours");
+                        } else if (text_view.equals(text_view8)) {
+                            text_view.setText((progress) + " hours");
+                        } else if (text_view.equals(text_view9)) {
+                            text_view.setText(Integer.toString(progress));
+                        }
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        if (text_view.equals(text_view5)) {
+                            text_view.setText((progress_value) + " oz");
+                        } else if (text_view.equals(text_view7)) {
+                            text_view.setText((progress_value) + " hours");
+                        } else if (text_view.equals(text_view8)) {
+                            text_view.setText((progress_value) + " hours");
+                        } else if (text_view.equals(text_view9)) {
+                            text_view.setText(Integer.toString(progress_value));
+                        }
+                    }
+                }
+        );
+
     }
 
     /**
