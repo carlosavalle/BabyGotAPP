fa2f25fe3db4c9e5a6b68350fab22c44fb434353
carlosavalle <carlosavalle@users.noreply.github.com>
2021-04-02 17:39:48 -0300

    Update UpdateStatsFragment.java
    + updated seekbar names
    + working on update seekbar according to the date and profile.

--- app/src/main/java/com/team6/ui/UpdateStatsFragment.java
+++ app/src/main/java/com/team6/ui/UpdateStatsFragment.java
@@ -3,6 +3,7 @@ package com.team6.ui;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,14 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.team6.GlobalVariable;
 import com.team6.NumbersUpdate;
 import com.team6.R;
@@ -32,19 +38,19 @@ import java.util.Map;
  */
 public class UpdateStatsFragment extends Fragment {
     @SuppressLint("StaticFieldLeak")
-    private static SeekBar seek_bar;
+    private static SeekBar seek_bar_milk;
     @SuppressLint("StaticFieldLeak")
     private static TextView text_view5;
     @SuppressLint("StaticFieldLeak")
-    private static SeekBar seek_bar2;
+    private static SeekBar seek_bar_tummytime;
     @SuppressLint("StaticFieldLeak")
     private static TextView text_view7;
     @SuppressLint("StaticFieldLeak")
-    private static SeekBar seek_bar3;
+    private static SeekBar seek_bar_sleep;
     @SuppressLint("StaticFieldLeak")
     private static TextView text_view8;
     @SuppressLint("StaticFieldLeak")
-    private static SeekBar seek_bar4;
+    private static SeekBar seek_bar_diapers;
     @SuppressLint("StaticFieldLeak")
     private static TextView text_view9;
     FirebaseDatabase database;
@@ -60,18 +66,18 @@ public class UpdateStatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View fragment = inflater.inflate(R.layout.fragment_update_stats, container, false);
-        seek_bar = fragment.findViewById(R.id.seekBar);
+        seek_bar_milk = fragment.findViewById(R.id.seekBar);
         text_view5 = fragment.findViewById(R.id.textView5);
-        seek_bar2 = fragment.findViewById(R.id.seekBar2);
+        seek_bar_tummytime = fragment.findViewById(R.id.seekBar2);
         text_view7 = fragment.findViewById(R.id.textView7);
-        seek_bar3 = fragment.findViewById(R.id.seekBar3);
+        seek_bar_sleep = fragment.findViewById(R.id.seekBar3);
         text_view8 = fragment.findViewById(R.id.textView8);
-        seek_bar4 = fragment.findViewById(R.id.seekBar4);
+        seek_bar_diapers = fragment.findViewById(R.id.seekBar4);
         text_view9 = fragment.findViewById(R.id.textView9);
-        seekBar(seek_bar, text_view5);
-        seekBar(seek_bar2, text_view7);
-        seekBar(seek_bar3, text_view8);
-        seekBar(seek_bar4, text_view9);
+        seekBar(seek_bar_milk, text_view5);
+        seekBar(seek_bar_tummytime, text_view7);
+        seekBar(seek_bar_sleep, text_view8);
+        seekBar(seek_bar_diapers, text_view9);
 
         // Save Data Button
         Button saveDataBtn = fragment.findViewById(R.id.save_data_btn);
@@ -79,10 +85,53 @@ public class UpdateStatsFragment extends Fragment {
         // Register all click events here instead of the fragment's .xml file.
         saveDataBtn.setOnClickListener(v -> saveData(v));
 
+        //select a date to retrieve Profile stats
+        CalendarView calendar = fragment.findViewById(R.id.calendarView4);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                String date = ""+(month +1)+"-"+dayOfMonth+"-" + year;
+                updateProgressBars(date);
+
+                Toast.makeText(getActivity().getBaseContext(), date, 0).show();// TODO Auto-generated method stub
+
+            }
+        });
+
         return fragment;
     }
 
+    public void updateProgressBars(String date) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("BabyProfiles").child("1").child("Stats").child("04-02-2021");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                NumbersUpdate numbersUpdate = snapshot.getValue(NumbersUpdate.class);
 
+                if(numbersUpdate != null) {
+                    seek_bar_milk.setProgress(Integer.parseInt(numbersUpdate.getMilk()));
+                    seek_bar_sleep.setProgress(Integer.parseInt(numbersUpdate.getSleep()));
+                    seek_bar_tummytime.setProgress(Integer.parseInt(numbersUpdate.getTummyTime()));
+                    seek_bar_diapers.setProgress(Integer.parseInt(numbersUpdate.getDiapers()));
+                }else{
+                    seek_bar_milk.setProgress(0);
+                    seek_bar_sleep.setProgress(0);
+                    seek_bar_tummytime.setProgress(0);
+                    seek_bar_diapers.setProgress(0);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("Data Load Error", "Could not load data from firebase");
+            }
+        });
+
+
+    }
     @SuppressLint("SetTextI18n")
     public void seekBar(SeekBar seekbar, TextView text_view) {
         if (text_view.equals(text_view5)) {
@@ -141,11 +190,11 @@ public class UpdateStatsFragment extends Fragment {
         String tummyTime = (String) text_view7.getText();
         String sleep = (String) text_view8.getText();
         String diapers = (String) text_view9.getText();
-        CalendarView test = getView().findViewById(R.id.calendarView4);
+        CalendarView calendar = getView().findViewById(R.id.calendarView4);
         database = FirebaseDatabase.getInstance();
         @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
-        String selectedDate = sdf.format(new Date(test.getDate()));
-        test.setOnDateChangeListener((CalendarView.OnDateChangeListener) (test1, year, month, dayOfMonth) -> {
+        String selectedDate = sdf.format(new Date(calendar.getDate()));
+        calendar.setOnDateChangeListener((CalendarView.OnDateChangeListener) (test1, year, month, dayOfMonth) -> {
             @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
             Date date = null;
             String MMM = "";
