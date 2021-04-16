b2754122bf5f7124577e4f62555670e8d6b3838b
johnleofraxidio <johnleofraxidio@gmail.com>
2021-04-16 19:58:38 +1000

    ..

--- app/src/main/java/com/team6/NumbersUpdate.java
+++ app/src/main/java/com/team6/NumbersUpdate.java
@@ -41,3 +41,175 @@ public class NumbersUpdate {
     }
 
 }
+
+package com.team6;
+
+
+public class NumbersUpdate {
+    private String id;
+
+    private String milk;
+    private String tummyTime;
+    private String sleep;
+    private String diapers;
+
+    public NumbersUpdate() {
+    }
+
+    public NumbersUpdate(String id, String milk, String tummyTime, String sleep, String diapers) {
+        this.id = id;
+        this.milk = milk;
+        this.tummyTime = tummyTime;
+        this.sleep = sleep;
+        this.diapers = diapers;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMilk() {
+        return milk;
+    }
+
+    public String getTummyTime() {
+        return tummyTime;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public String getDiapers() {
+        return diapers;
+    }
+
+}package com.team6;
+
+
+public class NumbersUpdate {
+    private String id;
+
+    private String milk;
+    private String tummyTime;
+    private String sleep;
+    private String diapers;
+
+    public NumbersUpdate() {
+    }
+
+    public NumbersUpdate(String id, String milk, String tummyTime, String sleep, String diapers) {
+        this.id = id;
+        this.milk = milk;
+        this.tummyTime = tummyTime;
+        this.sleep = sleep;
+        this.diapers = diapers;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMilk() {
+        return milk;
+    }
+
+    public String getTummyTime() {
+        return tummyTime;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public String getDiapers() {
+        return diapers;
+    }
+
+}
+
+package com.team6;
+
+
+public class NumbersUpdate {
+    private String id;
+
+    private String milk;
+    private String tummyTime;
+    private String sleep;
+    private String diapers;
+
+    public NumbersUpdate() {
+    }
+
+    public NumbersUpdate(String id, String milk, String tummyTime, String sleep, String diapers) {
+        this.id = id;
+        this.milk = milk;
+        this.tummyTime = tummyTime;
+        this.sleep = sleep;
+        this.diapers = diapers;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMilk() {
+        return milk;
+    }
+
+    public String getTummyTime() {
+        return tummyTime;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public String getDiapers() {
+        return diapers;
+    }
+
+}package com.team6;
+
+
+public class NumbersUpdate {
+    private String id;
+
+    private String milk;
+    private String tummyTime;
+    private String sleep;
+    private String diapers;
+
+    public NumbersUpdate() {
+    }
+
+    public NumbersUpdate(String id, String milk, String tummyTime, String sleep, String diapers) {
+        this.id = id;
+        this.milk = milk;
+        this.tummyTime = tummyTime;
+        this.sleep = sleep;
+        this.diapers = diapers;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMilk() {
+        return milk;
+    }
+
+    public String getTummyTime() {
+        return tummyTime;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public String getDiapers() {
+        return diapers;
+    }
+
+}
