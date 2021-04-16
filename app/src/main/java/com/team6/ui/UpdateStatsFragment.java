8a294109b489d22f0792b5134779d17107f7f3b5
johnleofraxidio <johnleofraxidio@gmail.com>
2021-04-16 20:00:43 +1000

    ..

--- app/src/main/java/com/team6/ui/UpdateStatsFragment.java
+++ app/src/main/java/com/team6/ui/UpdateStatsFragment.java
@@ -394,3 +394,43 @@ public class NumbersUpdate {
 
 }
 
+public class NumbersUpdate {
+    private String id;
+
+    private String milk;
+    private String tummyTime;
+    private String sleep;
+    private String diapers;
+
+    public NumbersUpdate() {
+    }
+
+    public NumbersUpdate(String id, String milk, String tummyTime, String sleep, String diapers) {
+        this.id = id;
+        this.milk = milk;
+        this.tummyTime = tummyTime;
+        this.sleep = sleep;
+        this.diapers = diapers;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getMilk() {
+        return milk;
+    }
+
+    public String getTummyTime() {
+        return tummyTime;
+    }
+
+    public String getSleep() {
+        return sleep;
+    }
+
+    public String getDiapers() {
+        return diapers;
+    }
+
+}
